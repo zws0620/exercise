ae5844d2b881595ceb4587c0c129f6ad43df92f0
DESKTOP-1DKSJRK\Administrator <zhh50@zhihaihe.cn>
2023-03-08 13:54:34 +0800

    提交新的分支

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,13 +1,115 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
-  </div>
+  <div id="container" style="width:300px; height: 180px;"></div>
+  <div id="zzz" class="about"></div>
 </template>
+
+<script setup>
+import * as echarts from 'echarts';
+import { onMounted } from 'vue';
+
+onMounted(() => {
+  // 基于准备好的dom，初始化echarts实例
+  var chartDom = document.getElementById('zzz');
+  var myChart = echarts.init(chartDom);
+  // 绘制图表
+  var option;
+
+  option = {
+    title: {
+      text: '未来一周气温变化'
+    },
+    tooltip: {
+      trigger: 'axis'
+    },
+    legend: {},
+    toolbox: {
+      show: true,
+      feature: {
+        dataZoom: {
+          yAxisIndex: 'none'
+        },
+        dataView: { readOnly: false },
+        magicType: { type: ['line', 'bar'] },
+        restore: {},
+        saveAsImage: {}
+      }
+    },
+    xAxis: {
+      type: 'category',
+      boundaryGap: false,
+      data: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
+    },
+    yAxis: {
+      type: 'value',
+      axisLabel: {
+        formatter: '{value} °C'
+      }
+    },
+    series: [
+      {
+        name: 'Highest',
+        type: 'line',
+        data: [10, 11, 13, 11, 12, 12, 9],
+        markPoint: {
+          data: [
+            { type: 'max', name: 'Max' },
+            { type: 'min', name: 'Min' }
+          ]
+        },
+        markLine: {
+          data: [{ type: 'average', name: 'Avg' }]
+        }
+      },
+      {
+        name: 'Lowest',
+        type: 'line',
+        data: [1, -2, 2, 5, 3, 2, 0],
+        markPoint: {
+          data: [{ name: '周最低', value: -2, xAxis: 1, yAxis: -1.5 }]
+        },
+        markLine: {
+          data: [
+            { type: 'average', name: 'Avg' },
+            [
+              {
+                symbol: 'none',
+                x: '90%',
+                yAxis: 'max'
+              },
+              {
+                symbol: 'circle',
+                label: {
+                  position: 'start',
+                  formatter: 'Max'
+                },
+                type: 'max',
+                name: '最高点'
+              }
+            ]
+          ]
+        }
+      }
+    ]
+  };
+
+  option && myChart.setOption(option);
+})
+
+// 高德-天气API
+window.onLoad = function () {
+  var map = new AMap.Map('container');
+}
+var url = 'https://webapi.amap.com/maps?v=1.4.15&key=8c3dadb148bb72594ff3531e0194d49a&callback=onLoad';
+var jsapi = document.createElement('script');
+// jsapi.charset = 'utf-8';
+jsapi.src = url;
+document.head.appendChild(jsapi);
+</script>
 
 <style>
 @media (min-width: 1024px) {
   .about {
-    min-height: 100vh;
+    min-height: 60vh;
     display: flex;
     align-items: center;
   }
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,36 @@
-<script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
-</script>
-
 <template>
-  <main>
-    <TheWelcome />
-  </main>
+  <div>
+    <h2>文档</h2>
+    <span>Vue 的
+      <a href="https://vuejs.org/" target="_blank" rel="noopener">
+        官方文档
+      </a>
+      为您提供了入门所需的所有信息。</span>
+  </div>
+  <div>
+    <h2>工具</h2>
+    <span>
+      该项目与
+      <a href="https://vitejs.dev/guide/features.html" target="_blank" rel="noopener">Vite</a>
+      一起提供服务和捆绑。推荐的 IDE 设置是
+      <a href="https://code.visualstudio.com/" target="_blank" rel="noopener">VSCode</a> +
+      <a href="https://github.com/johnsoncodehk/volar" target="_blank" rel="noopener">Volar</a>
+      。如果您需要测试组件和网页，请查看
+      <a href="https://www.cypress.io/" target="_blank" rel="noopener">Cypress</a> 和
+      <a href="https://on.cypress.io/component" target="_blank">Cypress Component Testing</a>
+      。有关更多说明，请参见。README.md
+    </span>
+  </div>
+  <div>
+    <h2>生态系统</h2>
+    <span>
+      获取项目的官方工具和库：
+      <a href="https://pinia.vuejs.org/" target="_blank" rel="noopener">Pinia</a>、
+      <a href="https://router.vuejs.org/" target="_blank" rel="noopener">Vue Router</a>、
+      <a href="https://test-utils.vuejs.org/" target="_blank" rel="noopener">Vue Test Utils</a>
+      和 <a href="https://github.com/vuejs/devtools" target="_blank" rel="noopener">Vue Dev Tools</a>
+      。 如果您需要更多资源，我们建议您访问
+      <a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">Awesome Vue</a>。
+    </span>
+  </div>
 </template>
